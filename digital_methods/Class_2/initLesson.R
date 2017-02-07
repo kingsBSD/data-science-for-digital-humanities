467b1fa53ae29dab288fc9cf1096bba154b6c078
Giles Richard Greenway <augeas@gmail.com>
2017-02-07 00:28:45 +0000

    Moved to sub-dir...

---
+++ digital_methods/Class_2/initLesson.R
@@ -0,0 +1,13 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+# Data
+
+days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+poker  <- c(140, -50, 20, -120, 240, -50, 20)
+roulette <- c(-24, -50, 100, -350, 10, 10, 100)
+happy<- c(FALSE,TRUE, FALSE, TRUE, FALSE, TRUE, FALSE)
